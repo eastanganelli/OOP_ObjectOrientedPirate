da9b76870bff7bc561a6667b48afe72a8192e2e2
eastanganelli <ezequielstanganelli@gmail.com>
2023-08-15 00:11:59 -0300

    Luffy checked

--- main.py
+++ main.py
@@ -30,37 +30,7 @@ def pirate() -> None:
         print(ship)
 
 if __name__ == "__main__":
-    url="https://my.api.mockaroo.com/oop_pirate.csv?key=44ac9940"
     try:
-        ShipLists = csv.reader(codecs.iterdecode(requests.get(url).iter_lines(), encoding="utf-8"), delimiter=",")
-    except (requests.exceptions.ConnectionError, requests.exceptions.RequestException):
-        ...
-    else:
-        myShips: list[any] = []
-        header: bool = True
-        for ship in ShipLists:
-            if not header:
-                try:
-                    if ship[2] == '' and ship[3] == '':
-                        myShips.append(Ship(int(ship[0]), int(ship[1])))
-                    elif ship[3] == '':
-                        myShips.append(Cruise(int(ship[2]), int(ship[0]), int(ship[1])))
-                    else:
-                        if ship[2] == '':
-                            continue
-                        myShips.append(Cargo(int(ship[2]), float(ship[3]), int(ship[0]), int(ship[1])))
-                except ValueError:
-                    continue
-            else:
-                header = False
-        
-        for ship_ in myShips:
-            try:
-                print(ship_.is_worth_it() >= 20)
-            except ValueError:
-                print("La carga no tiene valor")
-    
-    """try:
         ShipLists: any = read_pirate_notes()
     except FileNotFoundError:
         exit("No se encontró el archivo")
@@ -71,4 +41,4 @@ if __name__ == "__main__":
                 print(f"tiene un valor de {ship.is_worth_it()}")
             except ValueError:
                 print("La carga no tiene valor")
-            print("------------")"""
+            print("------------")
